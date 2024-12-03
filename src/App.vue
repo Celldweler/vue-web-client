54a6125d22c5ba4d5d7834f10a825eaf804e1f4e
Fedir Buzhylov <fedir.buzhylov@gmail.com>
2024-12-03 16:59:13 +0300

    clean up the solution

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,6 @@
 <script setup>
 import ChatRoom from './components/ChatRoom.vue';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-console.log("api url",apiUrl)
-
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
